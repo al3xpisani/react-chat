6ca600d2a3f606b7ed4c6789d160cf910641627d
ALEXANDRE PISANI ANTONIO <alexandre.antonio@MBMZN010030.mobly.local>
2021-08-02 10:12:16 -0300

    chat react chat-engine.io

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+import ChatFeed from "./components/ChatFeed";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    // if (!localStorage.getItem("username")) return <LoginForm />;
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID={process.env.REACT_APP_PROJECT_ID}
+            userName="javascriptmastery"
+            userSecret="123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() =>
+                new Audio(
+                    "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+                ).play()
+            }
+        />
+    );
+};
 
 export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,3 @@
+export { default as MessageForm } from "./MessageForm";
+export { default as MyMessage } from "./MyMessage";
+export { default as TheirMessage } from "./TheirMessage";
